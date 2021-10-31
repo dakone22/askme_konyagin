f23d1f75d65c472a6c4f1b487ea9e04fecb5dcbf
dakone22 <35692622+dakone22@users.noreply.github.com>
2021-10-31 04:15:41 +0300

    task3: rework fill db

--- askme/questions/management/commands/fill_db.py
+++ askme/questions/management/commands/fill_db.py
@@ -1,138 +1,283 @@
-import os
 import random
 import string
+import urllib.request
+import warnings
+from argparse import ArgumentParser
 
+import markovify
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
+from django.db import models
 from django.utils import timezone  # TODO: random datetime
 
 from questions.models import QuestionVote, AnswerVote
 from questions.models import Tag, Question, Answer
 
 
-def _get_str(chars, rang):
-    return ''.join([random.choice(chars) for _ in range(random.randint(*rang))])
+def benchmark(func):
+    def new_func(*args, **kwargs):
+        import time
+        import cProfile, pstats, io
+        import tracemalloc
+        from pstats import SortKey
+
+        start_time = time.time()
+
+        pr = cProfile.Profile()
+        pr.enable()
+        tracemalloc.start()
+
+        result = func(*args, **kwargs)
+
+        end_time = time.time()
+        snapshot = tracemalloc.take_snapshot()
+        pr.disable()
+
+        s = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+
+        top_stats = snapshot.statistics('lineno')
+        print("[ Top 10 ]")
+        for stat in top_stats[:10]:
+            print(stat)
+
+        print(f'Done in {end_time - start_time:.2f} seconds')
+
+        return result
+
+    return new_func
 
 
-def get_username():  # TODO: random usernames
-    return _get_str(string.ascii_letters, (5, 15))
+class Faker:
+    TEXT_DATA_URL = r'https://raw.githubusercontent.com/brunoklein99/deep-learning-notes/master/shakespeare.txt'
+    USERNAMES_URL = r'https://github.com/jeanphorn/wordlist/raw/master/usernames.txt'
+    DOMAINS_URL = r'https://raw.githubusercontent.com/fgont/domain-list/master/alexa-domains.txt'
+    LANGUAGES_URL = r'https://raw.githubusercontent.com/csurfer/gitlang/master/languages.txt'
+    WORDS_URL = r'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
 
+    def __init__(self):
+        self.text_model = markovify.Text('\n'.join(self._load_lines(Faker.TEXT_DATA_URL)))
+        self.username_list = self._load_lines(Faker.USERNAMES_URL)
+        self.domain_list = ['google.com']  # self._load_lines(Faker.DOMAINS_URL)
+        self.words_list = self._load_lines(Faker.WORDS_URL) + self._load_lines(Faker.LANGUAGES_URL)
 
-def get_email():
-    return _get_str(string.ascii_letters + string.digits, (5, 25)) + "@" + \
-           _get_str(string.ascii_lowercase, (5, 25)) + "." + _get_str(string.ascii_lowercase, (2, 4))
+    @staticmethod
+    def _load_lines(url: str, encoding='utf-8') -> list[str]:
+        print(f'Downloading {url}...')
+        file = urllib.request.urlopen(url)
 
+        result = []
+        for line in file:
+            try:
+                decoded_line = line.decode(encoding)
+            except UnicodeDecodeError:
+                print(line, repr(line))
+            else:
+                result.append(decoded_line.strip())
 
-def get_password():
-    return _get_str(string.printable, (5, 50))
+        return result
 
+    def username(self):
+        return random.choice(self.username_list)
 
-try:
-    import markovify
+    def domain(self):
+        return random.choice(self.domain_list)
 
-    with open(os.path.join(__file__, '..', 'data.txt')) as f:
-        text = f.read()
-    text_model = markovify.Text(text)
+    def email(self):
+        return self.username() + '@' + self.domain()
 
-    def get_title():
-        return text_model.make_short_sentence(100, 10)
-
-
-    def get_text():
+    @staticmethod
+    def _safe_markov(get: callable):
         while True:
-            text = text_model.make_short_sentence(750, 300)
-            if text: return text
-except ImportError:
-    get_title = get_username
+            if text := get():
+                return text
+            warnings.warn("Markov return a null!")
+
+    def title(self):
+        return self._safe_markov(lambda: self.text_model.make_short_sentence(100, 10))
+
+    def text(self):
+        return self._safe_markov(lambda: self.text_model.make_short_sentence(750, 300))
+
+    def tag(self):
+        return random.choice(self.words_list)
+
+    @staticmethod
+    def chars(charset: str, min: int, max: int):
+        return ''.join([random.choice(charset) for _ in range(random.randint(min, max))])
+
+    @staticmethod
+    def password():
+        return Faker.chars(string.printable, 5, 50)
+
+    @staticmethod
+    def datetime(start: timezone.datetime, end: timezone.datetime = None):
+        end = end or timezone.now()
+        assert start < end
+        delta = timezone.timedelta(seconds=random.randint(1, round((end - start).total_seconds())))
+        return start + delta
 
 
-    def get_text():
-        return _get_str(string.ascii_letters + ' ' * 20, (750, 300))
+class Filler:
+    def __init__(self,
+                 faker: Faker,
+                 users: int,
+                 tags: int,
+                 questions: int,
+                 tags_per_question: int,
+                 answers: int,
+                 question_votes: int,
+                 answer_votes: int,
+                 ):
+        self.faker = faker
+        self.users = users
+        self.tags = tags
+        self.questions = questions
+        self.tags_per_question = tags_per_question
+        self.answers = answers
+        self.question_votes = question_votes
+        self.answer_votes = answer_votes
 
-COUNTS = {
-    'users': 30,
-    'tags': 30,
-    'questions': 30,
-    'tags_link': 10,
-    'answers': 15,
-    'QuestionVote': 100,
-    'AnswerVote': 200,
-}
+    @staticmethod
+    def _fill(model: type[models.Model], count: int, create: callable, after: callable = None):
+        cur = model.objects.count()
+        print(f"Filling {model} [{cur}/{count}]...")
+        for i in range(count - cur):
+            obj = create(i)
+            print(f'({i}) New {obj}')
+            obj.save()
 
+            if after is not None:
+                after(obj)
+        print()
 
-# COUNTS = {
-#     'users': 10_000,
-#     'tags': 10_000,
-#     'questions': 100_000,
-#     'tags_link': 15,
-#     'answers': 1_000_000,
-#     'QuestionVote': 1_000_000,
-#     'AnswerVote': 1_000_000,
-# }
+    @staticmethod
+    def get_start_date():
+        return timezone.now() - timezone.timedelta(days=7)
+
+    @property
+    def start_date(self):
+        return self.get_start_date()
+
+    @benchmark
+    def fill(self):
+        self.fill_users()
+        self.fill_tags()
+
+        self.fill_questions()
+
+        self.fill_answers()
+        self.fill_votes()
+
+    def fill_users(self):
+        self._fill(
+            model=User,
+            count=self.users,
+            create=lambda i: User.objects.create_user(username=self.faker.username(),
+                                                      email=self.faker.email(),
+                                                      password=self.faker.password())
+        )
+
+    def fill_tags(self):
+        self._fill(
+            model=Tag,
+            count=self.tags,
+            create=lambda i: Tag.objects.create(name=self.faker.tag(), color_id=random.randint(0, 5))
+        )
+
+    def fill_questions(self):
+        def after(q: Question):
+            tag_count = random.randint(1, self.tags_per_question)
+            # tags = random.sample(Tag.objects.all(), tag_count)  # TODO: check memory usage !!!
+            tags = [random.choice(Tag.objects.all()) for _ in range(tag_count)]
+            print(f"Adding tags: {tags}")
+            for tag in tags:
+                q.tags.add(tag)
+
+        self._fill(
+            model=Question,
+            count=self.questions,
+            create=lambda i: Question.objects.create(
+                author=random.choice(User.objects.all()),  # TODO: check memory usage
+                date=self.faker.datetime(self.start_date),
+                title=self.faker.title(),
+                text=self.faker.text(),
+            ),
+            after=after,
+        )
+
+    def fill_answers(self):
+        self._fill(
+            model=Answer,
+            count=self.answers,
+            create=lambda i: Answer.objects.create(
+                author=random.choice(User.objects.all()),  # TODO: check memory usage
+                question=random.choice(Question.objects.all()),  # TODO: check memory usage
+                date=self.faker.datetime(self.start_date),
+                text=self.faker.text(),
+            )
+        )
+
+    def fill_votes(self):
+        self._fill(
+            model=QuestionVote,
+            count=self.question_votes,
+            create=lambda i: QuestionVote.objects.create(
+                user=random.choice(User.objects.all()),  # TODO: check memory usage
+                question=random.choice(Question.objects.all()),  # TODO: check memory usage
+                type=random.randint(0, 100) > 10,
+            )
+        )
+
+        self._fill(
+            model=AnswerVote,
+            count=self.answer_votes,
+            create=lambda i: AnswerVote.objects.create(
+                user=random.choice(User.objects.all()),  # TODO: check memory usage
+                answer=random.choice(Answer.objects.all()),  # TODO: check memory usage
+                type=random.randint(0, 100) > 20,
+            )
+        )
 
 
 class Command(BaseCommand):
-    help = 'Generate start up DB'
+    help = 'Fill DB with random data'
+
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument(
+            '--big',
+            help='A looot of data',
+            action='store_true',
+        )
 
     def handle(self, *args, **options):
-        print("Creating users...")
-        for _ in range(COUNTS['users']):
-            u = User.objects.create_user(username=get_username(), email=get_email(), password=get_password())
-            print(f"New user {u}")
-            u.save()
+        quantities = {
+            'users': 10_000,
+            'tags': 10_000,
+            'questions': 100_000,
+            'tags_per_question': 15,
+            'answers': 1_000_000,
+            'question_votes': 1_000_000,
+            'answer_votes': 1_000_000,
+        } if options['big'] else {
+            'users': 30,
+            'tags': 30,
+            'questions': 30,
+            'tags_per_question': 10,
+            'answers': 15,
+            'question_votes': 100,
+            'answer_votes': 200,
+        }
 
-        print("Creating tags...")
-        for _ in range(COUNTS['users']):
-            t = Tag.objects.create(name=f"Tag{_}", color_id=random.randint(0, 5))
-            print(f"New tag {t}")
-            t.save()
+        print("Initing faker...")
+        faker = Faker()
 
-        print("Creating questions...")
-        for _ in range(COUNTS['questions']):
-            q = Question.objects.create(
-                author=random.choice(User.objects.all()),
-                date=timezone.now(),
-                title=get_title(),
-                text=get_text(),
-            )
-            print(f"New question {q}")
-            q.save()
+        print("Filling DB with:")
+        print('\n'.join([f"{key} = {value}" for key, value in quantities.items()]))
 
-            print("linking tags...")
-            for _ in range(random.randint(1, COUNTS['tags_link'])):
-                while True:
-                    print(f"tag {_}")
-                    tag = random.choice(Tag.objects.all())
-                    if q not in tag.questions():
-                        q.tags.add(tag)
-                        print(f"new tag link {q} to {tag}")
-                        break
+        filler = Filler(faker, **quantities)
 
-            print("creating answers...")
-            for _ in range(random.randint(1, COUNTS['answers'])):
-                a = Answer.objects.create(
-                    author=random.choice(User.objects.all()),
-                    question=q,
-                    date=timezone.now(),
-                    text=get_text(),
-                )
-                print(f"new answer {a}")
-                a.save()
-
-        print("Creating votes")
-        for _ in range(COUNTS['QuestionVote']):
-            v = QuestionVote.objects.create(
-                user=random.choice(User.objects.all()),
-                question=random.choice(Question.objects.all()),
-                type=random.randint(0, 100) > 40,
-            )
-            print(f"created {v}")
-            v.save()
-        for _ in range(COUNTS['AnswerVote']):
-            v = AnswerVote.objects.create(
-                user=random.choice(User.objects.all()),
-                answer=random.choice(Answer.objects.all()),
-                type=random.randint(0, 100) > 40,
-            )
-            print(f"created {v}")
-            v.save()
+        filler.fill()
